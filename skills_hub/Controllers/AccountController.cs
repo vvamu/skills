b407f7706c68c11d764d7e912ecea9c1ed2cd02b
vvamu <nadezhda.bn.02@mail.ru>
2025-02-03 10:37:47 +0300

    init

---
+++ skills_hub/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace skills_hub.Controllers;
+
+public class AccountController : Controller
+{
+    public async Task<IActionResult> Item()
+    {
+        return View("~/Views/Account/Item/Item.cshtml"); 
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        return View("Create");
+    }
+    public async Task<IActionResult> SignIn()
+    {
+        return View("SignIn");
+    }
+}
